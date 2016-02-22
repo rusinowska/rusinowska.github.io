63e8aeaba3b49660df8136fb67916cc5a1c0f257
Zofia Rusinowska <zosia.rusinowska@gmail.com>
2016-02-22 14:36:25 +0100

    Akwarele koncept 1

---
+++ jscript.js
@@ -0,0 +1,11 @@
+/* global $ */
+//window.viewportUnitsBuggyfill.init();
+
+$(document).ready(function() {
+    $('#zr').click(function(){
+        $('#zr').animate({left: '250px'});
+        
+        
+    });
+    
+});
