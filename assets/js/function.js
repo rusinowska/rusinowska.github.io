8659a67ef6d7d6a356d50688a607fa9416c77139
Zofia Rusinowska <zosia.rusinowska@gmail.com>
2016-11-18 17:08:20 +0100

    add

--- assets/js/function.js
+++ assets/js/function.js
@@ -5,4 +5,16 @@ $(document).ready(function() {
        slideMove:0,
        pager:true
    });
+
+
+   $(function() {
+       $('a.page-scroll').bind('click', function(event) {
+           var $anchor = $(this);
+           $('html, body').stop().animate({
+               scrollTop: $($anchor.attr('href')).offset().top
+           }, 1500, 'easeInOutExpo');
+           event.preventDefault();
+       });
+   });
+
  });
