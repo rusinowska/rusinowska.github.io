b4e6b59a59e9bb9dd0e0e7dd49e7ae022e0f8ba6
“Zofia <zofia.rusinowska@pl.abb.com>
2018-05-09 23:30:09 +0200

    font change

--- resources/go-fucking-work.js
+++ resources/go-fucking-work.js
@@ -1,7 +1,12 @@
 var quotes = [
-  "CHRZAŃ UFNĄ BELGIJSKOŚĆ, TĘP WYŻ-MŁÓDŹ",
-  "JEŻU KLĄTW, SPŁÓDŹ FINOM CZĘŚĆ GRY HAŃB",
-  ""
+  "CHRZAŃ UFNĄ BELGIJSKOŚĆ, TĘP WYŻ-MŁÓDŹ!",
+  "JEŻU KLĄTW, SPŁÓDŹ FINOM CZĘŚĆ GRY HAŃB!",
+  "FAŁSZ TRWÓG: JUDŹ KLOMB, NĘĆ CHYŻĄ PIEŚŃ!",
+  "FILMUJ RZEŹ ŻĄDAŃ, POŚĆ, GNĘB CHŁYSTKÓW!",
+  "MAŃKUT, JEŚĆ! WŁÓŻ PCHLI SZYFON, GĘDŹ, RĄB!",
+  "PÓJDŹ W LOCH ZABIĆ MAŁEŃSKĄ GĘŚ FUTRYN!",
+  "O, MÓGŁŻE SĘP CHLAŃ WYJŚĆ FURTKĄ BŹDZIN!",
+  "SKŁÓĆ BAŚŃ GRYPĄ, ZAWĘDŹ FLEJTUCHOM NIŻ!"
 ];
 
 function getQuote () {
